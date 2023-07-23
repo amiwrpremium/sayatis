1f70a232741028ac8def68d9e0468ef4db2594be
AMiWR <amiwrpremium@gmail.com>
2023-07-23 15:21:31 +0330

    pwa

--- sayatis/sayatis/settings.py
+++ sayatis/sayatis/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'pwa',
     'home'
 ]
 
@@ -113,7 +114,6 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = '/var/www/static'
 
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
@@ -121,3 +121,51 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+PWA_APP_NAME = 'Sayatis'
+PWA_APP_DESCRIPTION = "Sayatis' website"
+PWA_APP_THEME_COLOR = '#0A0302'
+PWA_APP_BACKGROUND_COLOR = '#ffffff'
+PWA_APP_DISPLAY = 'standalone'
+PWA_APP_SCOPE = '/'
+PWA_APP_ORIENTATION = 'any'
+PWA_APP_START_URL = '/'
+PWA_APP_STATUS_BAR_COLOR = 'default'
+PWA_APP_ICONS = [
+    {
+        'src': '/static/img/logo-bg.png',
+        'sizes': '160x160'
+    }
+]
+PWA_APP_ICONS_APPLE = [
+    {
+        'src': '/static/img/logo-bg.png',
+        'sizes': '160x160'
+    }
+]
+PWA_APP_SPLASH_SCREEN = [
+    {
+        'src': '/static/img/logo-bg.png',
+        'media': '(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)'
+    }
+]
+PWA_APP_DIR = 'ltr'
+PWA_APP_LANG = 'en-US'
+PWA_APP_SHORTCUTS = [
+    {
+        'name': 'Shortcut',
+        'url': '/target',
+        'description': 'Shortcut to a page in my application'
+    }
+]
+PWA_APP_SCREENSHOTS = [
+    {
+        'src': '/static/img/logo-bg.png',
+        'sizes': '750x1334',
+        "type": "image/png"
+    }
+]
