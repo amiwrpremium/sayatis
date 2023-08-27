f587c3e35fa216f9cb86df9bfe8281026b2298bb
AMiWR <amiwrpremium@gmail.com>
2023-08-27 16:09:30 +0330

    add lang button

--- sayatis/home/urls.py
+++ sayatis/home/urls.py
@@ -11,5 +11,5 @@ app_name = "home"
 
 urlpatterns = [
     path("", views.HomeView.as_view(), name="index"),
-    path("fa/", views.FaView.as_view(), name="index"),
+    path("fa/", views.FaView.as_view(), name="index-fa"),
 ]
